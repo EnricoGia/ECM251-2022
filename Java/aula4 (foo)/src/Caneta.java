1b9091777168d80c851b570532ac4e1c9d1458ed
EnricoGia <rico500000@gmail.com>
2022-03-09 11:12:38 -0300

    Criação de classes com Java

---
+++ Java/aula4 (foo)/src/Caneta.java
@@ -0,0 +1,16 @@
+public class Caneta{
+
+    //Características da caneta
+    String modelo;
+    String cor;
+    double ponta;
+    int carga; //(0-100);
+
+    //Comportamentos da caneta (métodos)
+
+    void Escrever(String texto){
+        System.out.println(texto);
+
+    }
+
+}
