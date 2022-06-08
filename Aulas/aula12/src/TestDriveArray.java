4cdfdd889e63f48668ec60d0c3b46b079b291d77
Enrico <rico500000@gmail.com>
2022-06-08 14:39:09 -0300

    aula12 (collections p.1)

---
+++ Aulas/aula12/src/TestDriveArray.java
@@ -0,0 +1,12 @@
+public class TestDriveArray{
+    public static void main(String[] args){
+        int[] meuArray;
+
+        meuArray = new int[4];
+
+        meuArray[0] = 45;
+        meuArray[1] = 12;
+        meuArray[2] = 9;
+        meuArray[3] = 5;
+    }
+}
---
+++ Aulas/aula12/src/TestDriveArrayList.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+
+public class TestDriveArrayList {
+    public static void main(String[] args){
+        ArrayList canetas = new ArrayList<Caneta>();
+        canetas.add(new Caneta("Azul" , 0.7));
+        canetas.add(new Caneta("Vermelha" , 1));
+
+        for(int i = 0; i < canetas.size(); i++){
+
+        }
+    }
+    
+}
