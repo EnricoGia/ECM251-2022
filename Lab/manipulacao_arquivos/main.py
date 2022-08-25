22386528bda6ead68da9d42391daf6a1b5115427
ENRICO GIANNOBILE <19.00610-0@maua.br>
2022-08-25 10:58:45 -0300

    Tratando exceções

--- Lab/manipulacao_arquivos/main.py
+++ Lab/manipulacao_arquivos/main.py
@@ -1,13 +1,25 @@
+from traceback import print_stack
 
 # Manipulação básica de arquivos
 
-arquivo = open("data/sistemas.txt", "a")
-continuar = True
-while continuar:
-    os_name = input("Diga um OS (vazio para sair): ")
-    if not os_name:
-        continuar = False
-        continue
-    arquivo.write(os_name + "\n")
+try:
+    arquivo = open("data/sistemas.txt", "a")
+    continuar = True
+    while continuar:
+        os_name = input("Diga um OS (vazio para sair): ")
+        if not os_name:
+            continuar = False
+            continue
+        arquivo.write(os_name + "\n")
 
-arquivo.close()
+    arquivo.close()
+
+except FileNotFoundError:
+    print("Caminho não existe, favor verificar")
+except Exception :
+    print("Algo não esperado ocorreu:")
+    print_stack() 
+finally:
+    print("Chegamos no final!")
+    
+
