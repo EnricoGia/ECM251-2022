2c2966dc08227e4d2d125c588f764bb6ffa78a76
Enrico <rico500000@gmail.com>
2022-08-27 17:31:39 -0300

    Separando rotinas em funções e começando opções

--- Lab/Login/main.py
+++ Lab/Login/main.py
@@ -2,31 +2,39 @@
 
 arquivo_nome = "logs.txt"
 
-
 # Criação e checagem de novo usuário
 
-arquivo = open(arquivo_nome, "r")
 
-usuario_nome = input("Digite um novo nome de usuário: ")
+def novo_usuario():
 
-linhas = arquivo.readlines()
+    usuario_nome = input("Digite um novo nome de usuário: ")
 
-novo = True
+    arquivo = open(arquivo_nome, "r")
+    linhas = arquivo.readlines()
+    novo = True
 
-for linha in linhas:
-    if (usuario_nome == linha.strip().split(":")[0]):
-        print("Usuário já existe!")
-        novo = False
-        break
+    for linha in linhas:
+        if (usuario_nome == linha.strip().split(":")[0]):
+            print("Usuário já existe!")
+            novo = False
+            break
 
-arquivo.close()
+    arquivo.close()
 
-if (novo):
-    senha = input("Digite uma senha: ")
-    arquivo = open(arquivo_nome, "a")
-    arquivo.write(usuario_nome + ":" + senha + "\n")
-    print("Usuário %s criado." % usuario_nome)
+    if (novo):
+        senha = input("Digite uma senha: ")
+        arquivo = open(arquivo_nome, "a")
+        arquivo.write(usuario_nome + ":" + senha + "\n")
+        print("Usuário %s criado." % usuario_nome)
 
-arquivo.close()
-###
+    arquivo.close()
+
+def teste():
+    print("Funcionando...")
+### Main
+
+x = {1:novo_usuario,2:teste}
+
+x[2]()
+
 print("Finalizando...")
