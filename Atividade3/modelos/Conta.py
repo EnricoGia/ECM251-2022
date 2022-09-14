b80b6c9e91d56c6b60adde6dfab2329a124fc87f
Enrico <rico500000@gmail.com>
2022-09-14 16:29:40 -0300

    Organizando bootstrap e Streamlit

--- Atividade3/modelos/Conta.py
+++
@@ -1,9 +0,0 @@
-class Conta():
-    def __init__(self,usuario,senha):
-        self._usuario = usuario
-        self._senha = senha
-
-    def get_usuario(self):
-        return self._usuario
-    
-    
